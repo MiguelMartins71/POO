5646c4b6a497e0cd9f0bf421ba1f2a3dce0a7fa1
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-08 12:17:24 -0300

    Add files via upload

---
+++ ExercicioIdade/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa a = new Pessoa();
+            Pessoa b = new Pessoa();
+
+            Console.WriteLine("Digite os dados da primeira pessoa");
+            a.nome = Console.ReadLine();
+            a.idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome da segunda pessoa");
+            b.nome = Console.ReadLine();
+            b.idade = int.Parse(Console.ReadLine());
+
+            if (a.idade > b.idade)
+            {
+
+                Console.WriteLine("Pessoa mais velha: {0}",a.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: {0}",b.nome);
+            }
+
+        }
+    }
+}

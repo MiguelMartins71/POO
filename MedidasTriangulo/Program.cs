22c7ea181d55f302b4d5c9f6da6c9975ad77bed3
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-10 23:23:51 -0300

    Add files via upload

---
+++ MedidasTriangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Xml;
+namespace MedidasTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+         
+            Console.WriteLine("Entre a largura e altura do retângulo");
+            Console.Write("Altura");
+            x.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Largura");
+            x.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = "+x.Area());
+            Console.WriteLine("Perimetro ="+x.Perimetro());
+            Console.WriteLine("DIAGONAL = "+x.Diagonal());
+            
+
+
+        }
+    }
+}
---
+++ MedidasTriangulo/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidasTriangulo
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura - Altura);
+
+        }
+        public double Diagonal()
+        {
+
+            return Math.Sqrt(Math.Pow(Largura, 2) + (Math.Pow(Altura,2)));
+        }
+
+        public override string ToString()
+        {
+            return "Larugra" + Largura + "Altura" + Altura;
+        }
+         
+    }
+}

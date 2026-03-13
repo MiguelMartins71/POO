5272ce3fe88d0b572fef7c1a57a875194cfa39b8
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-12 23:04:42 -0300

    Add files via upload

---
+++ MembroEstat/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembroEstat
+{
+    internal class Calculadora
+    {
+        public static double Pi = 3.14;
+        public double teste;
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+
+        public static int tax(int r)
+        {
+            return tax * 0.06;
+        }
+    }
+}

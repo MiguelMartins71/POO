874628db9bf21cb4502cced38862f695c73c32b9
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-15 22:48:04 -0300

    Add files via upload

---
+++ Banco/DadosBancarios.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class DadosBancarios
+    {
+        private int _id;
+        private string _name { get; set; }
+        private double _deposito {  get; set; }
+
+
+        public DadosBancarios()
+        {
+
+        }
+
+        public DadosBancarios(int numero, string name, double valor)
+        {
+            _id = numero;
+            _name = name;   
+            _deposito = valor;
+
+        }
+
+        public int id
+        {
+
+            get { return _id; }
+            set { 
+            if(value>0)
+                {
+                    _id = value;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida");
+                }
+                 
+            }
+        }
+
+        
+        public void AdicionarDeposito(int depositar)
+        {
+            _deposito += depositar;
+        }
+
+        public void Sacar(int saque)
+        {
+            _deposito -= saque;
+        }
+
+        public double ValorTotal()
+        {
+           return _deposito;
+        }
+
+        public override string ToString()
+        {
+            return "Conta, " + _id + " Titular: " + _name + "Saldo"  + ValorTotal().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            char opcao;
+         
+            Console.WriteLine("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial? s/n");
+            opcao = char.Parse
+                (Console.ReadLine());
+            Console.WriteLine("Entre o valor do depósito incial");
+            double valor = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados da Conta/n Conta {0}, Titular: {1}, Saldo: {2}",numero,nome,valor);
+
+            DadosBancarios banco = new DadosBancarios(numero, nome, valor);
+
+            if(opcao == 's')
+            {
+
+                Console.WriteLine("Entre com um valor para deposito");
+                int novodeposito = int.Parse(Console.ReadLine());
+                banco.AdicionarDeposito(novodeposito);
+
+
+
+            }
+
+            Console.WriteLine(banco);
+
+            Console.WriteLine("Entre com um valor para saque: ");
+            int novosaque = int.Parse(Console.ReadLine());
+            banco.Sacar(novosaque);
+
+            Console.WriteLine("Dados atualizados: "+banco);
+
+        }
+    }
+}

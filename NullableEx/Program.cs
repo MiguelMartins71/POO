8e4dbf8a9131ac6d6fa52f090ff84cca19d393c9
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-16 13:33:40 -0300

    Add files via upload

---
+++ NullableEx/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Nullable<double> x = null;
+            double? x = null;
+            double? y = 10.0;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+            Console.WriteLine();
+            double teste = x ?? 1102;
+            Console.WriteLine(teste);
+
+            if(x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X é nulo");
+
+            if(y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("errado");
+        }
+    }
+}
---
+++ NullableEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("NullableEx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NullableEx")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("7a670cfa-e6c8-48d7-9a4a-4a5942e52fb0")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

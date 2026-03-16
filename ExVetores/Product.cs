b1e8371a50762b5f1ba026af5d8b9738f2fa9320
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-16 18:08:51 -0300

    Add files via upload

---
+++ ExVetores/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExVetores
+{
+    internal class Product
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product (string nome, double preco)
+        {
+            Name = nome;
+            Price = preco;
+
+        }
+
+
+
+    }
+}

fb8a9faf0c3a60a2a031f67f179eb4e2a0f11b8b
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-26 22:21:52 -0300

    Add files via upload

---
+++ ForeachSX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachSX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Miguel", "Teste" };
+
+            foreach (string s in vect)
+            {
+                Console.WriteLine(s);
+
+            }
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+
+                Console.WriteLine(vect[i]);
+            }
+        }
+    }
+}

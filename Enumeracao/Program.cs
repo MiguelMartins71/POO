d949147456f70d912237de8f8f0a57c4be25cf14
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-05-03 12:41:28 -0300

    Add files via upload

---
+++ Enumeracao/Program.cs
@@ -0,0 +1,24 @@
+﻿using Enumeracao.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+        }
+    }
+}
